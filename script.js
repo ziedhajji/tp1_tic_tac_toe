9715c734dd42e46d4ab801c8a683ac0cad895957
ziedhajji <ziedhajji27@gmail.com>
2021-05-11 00:47:06 -0400

    2 eme mis a jours

--- script.js
+++ script.js
@@ -63,11 +63,11 @@ function jouer(element)
         alert("Message d'erreur");
 
 
-    Xgagne();
+    XOgagne();
 
 }
 
-function Xgagne() {
+function XOgagne() {
 
     let c1 = document.getElementById("c1").innerHTML;
     let c2 = document.getElementById("c2").innerHTML;
@@ -79,71 +79,53 @@ function Xgagne() {
     let c8 = document.getElementById("c8").innerHTML;
     let c9 = document.getElementById("c9").innerHTML;
     let gagne = document.getElementById("gagne");
-    alert(c1 + c2 + c3);
+
     let etat = 0;
 
-    if ((c1 == c2) && (c2 == c3) && (c3 == "X" || c3 == "O")) {
-        if (c3 == "X") etat = 1;
+    if ((c1 === c2) && (c2 === c3) && (c3 === "X" || c3 === "O")) {
+        if (c3 === "X") etat = 1;
         else   etat = 2;
     }
-    if ((c4 == c5) && (c5 == c6) && (c6 == "X" || c6 == "O")) {
-        if (c6 == "X") etat = 1;
+    else if ((c4 === c5) && (c5 === c6) && (c6 === "X" || c6 === "O")) {
+        if (c6 === "X") etat = 1;
         else  etat = 2;
     }
-    if ((c7 == c8) && (c8 == c9) && (c9 == "X" || c9 == "O")) {
-        if (c9 == "X")   etat = 1;
+    else if ((c7 === c8) && (c8 === c9) && (c9 === "X" || c9 === "O")) {
+        if (c9 === "X")   etat = 1;
         else  etat = 2;
     }
-    if ((c1 == c4) && (c4 == c7) && (c7 == "X" || c7 == "O")) {
-        if (c7 == "X") etat = 1;
+    else  if ((c1 === c4) && (c4 === c7) && (c7 === "X" || c7 === "O")) {
+        if (c7 === "X") etat = 1;
         else  etat = 2;
     }
-    if ((c2 == c5) && (c5 == c8) && (c8 == "X" || c8 == "O")) {
-        if (c8 == "X") etat = 1;
+    else if ((c2 === c5) && (c5 === c8) && (c8 === "X" || c8 === "O")) {
+        if (c8 === "X") etat = 1;
         else  etat = 2;
     }
-    if ((c3 == c6) && (c6 == c9) && (c9 == "X" || c9 == "O")) {
+    else if ((c3 === c6) && (c6 === c9) && (c9 === "X" || c9 === "O")) {
         if (c9 == "X") etat = 1;
         else  etat = 2;
     }
-    if ((c1 == c5) && (c5 == c9) && (c9 == "X" || c9 == "O")) {
-        if (c9 == "X") etat = 1;
+    else if ((c1 === c5) && (c5 === c9) && (c9 === "X" || c9 === "O")) {
+        if (c9 === "X") etat = 1;
         else  etat = 2;
+
     }
-    if ((c3 == c5) && (c5 == c9) && (c7 == "X"|| c7 == "O")) {
-        if (c7 == "X") etat = 1;
+    else if ((c3 === c5) && (c5 === c7) && (c7 === "X"|| c7 === "O")) {
+        if (c7 === "X") etat = 1;
         else  etat = 2;
     }
 
 
-    if (etat == 1)
+    if (etat === 1)
     {
         gagne.innerHTML = "X gagne!";
-        gagne.setAttribute("class", "jeuxTicTac2");
+        gagne.setAttribute("class", "gagne");
     }
-    else if(etat==2)
+    else if(etat===2)
     {
         gagne.innerHTML = "O gagne!";
-        gagne.setAttribute("class", "jeuxTicTac2");
+        gagne.setAttribute("class", "gagne");
     }
 }
 
-/*reste a determiner en cas d'egalité l'image correspondante
-* supprimé la fonction onclick en cas ou l'un des deux joueurs gagne*/
-
-
-
-/*  if(tab[i]=="" ) {
-      tab[i]="X";
-      element.innerHTML = tab[i];
-      element.setAttribute("class","rouge");
-      i++;
-  }
-  else {
-      if (tab[i] == "" ) {
-          tab[i] = "O";
-          element.innerHTML = tab[i];
-          element.setAttribute("class", "bleu");
-          i++;
-      }
-  }*/
